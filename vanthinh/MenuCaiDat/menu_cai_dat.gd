eead88e3397ad97df46ef7432af5fe81d94c4efc
ThinhDola999 <nguyenvanthinh228866@gmail.com>
2023-11-06 17:25:25 +0700

    add file vanthinh gom: trang_chu, menuCaiDat

---
+++ vanthinh/MenuCaiDat/menu_cai_dat.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var menuSetting
+var trangChuNode
+
+func _ready():
+	trangChuNode = get_node("/root/TrangChu")
+	menuSetting = trangChuNode.get_node("MenuCaiDat")
+	
+func _on_button_close_pressed():
+	if menuSetting.is_visible():
+		menuSetting.hide()
+	else:
+		menuSetting.show()
